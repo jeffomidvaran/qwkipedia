acf4c00985f6c579238e4a5e531ca297530bc543
anaba-max <a.abadian@gmail.com>
2021-04-18 21:33:30 -0700

    User Authentication added

--- qwkipedia_iOS/Controller/LoginViewController.swift
+++ qwkipedia_iOS/Controller/LoginViewController.swift
@@ -10,5 +10,10 @@ import Firebase
 
 class LoginViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
     
 }
--- qwkipedia_iOS/Controller/RegisterViewController.swift
+++ qwkipedia_iOS/Controller/RegisterViewController.swift
@@ -10,11 +10,21 @@ import Firebase
 
 class RegisterViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var emailTextfield: UITextField!
+    
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBAction func createPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e)
+                } else {
+                    self.performSegue(withIdentifier: K.creatoInterstSegue, sender: self)
+                }
+            }
+        }
     }
-
-
+    
 }
 
