209596e37ba370eed32d4b869b3c370f5284bee2
Jeff Omidvaran <jeffomidvaran@gmail.com>
2021-04-22 14:57:49 -0600

    adding early main topic page

--- qwkipedia_iOS/qwkipedia/ViewControllers/TopicPageViewController.swift
+++ qwkipedia_iOS/qwkipedia/ViewControllers/TopicPageViewController.swift
@@ -7,25 +7,83 @@
 
 import UIKit
 
-class TopicPageViewController: UIViewController {
-
-    @IBOutlet weak var label: UILabel!
+class TopicPageViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
+    
+    let headerReuseIdentifier = "topicPageHeaderCell"
+    let qwkDescriptionReuseIdentifier = "qwkDescriptionCell"
+    let videoReuseIdentifier = "topicPageVideoCell"
+    let imageReuseIdentifier = "topicPageImageCell"
+    let audioReuseIdentifier = "topicPageAudioCell"
+    let discussionReuseIdentifier = "topicPageDiscussionCell"
+    let expertSummaryReuseIdentifier = "topicPageExpertSummaryCell"
+    let externalLinksReuseIdentifier = "topicPageExternalLinksCell"
+    let qwkRecommendationsReuseIdentifier = "topicPageQwkRecomnedationsCell"
+    
+    
+    @IBOutlet weak var topicPageCollectionView: UICollectionView!
     var text = "Nothing Set"
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = text
-        // Do any additional setup after loading the view.
+        let layout = topicPageCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: view.frame.width - 16, height: 144.5)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        switch indexPath[1]{
+        case 0:
+            print("Header")
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: headerReuseIdentifier , for: indexPath) as! UICollectionViewCell
+            return cell
+        case 1:
+            print("QwkDescription")
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: qwkDescriptionReuseIdentifier , for: indexPath) as! UICollectionViewCell
+            return cell
+        case 2:
+            print("Video")
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: videoReuseIdentifier , for: indexPath) as! UICollectionViewCell
+            return cell
+        case 3:
+            print("Image")
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: imageReuseIdentifier , for: indexPath) as! UICollectionViewCell
+            return cell
+        case 4:
+            print("Audio")
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: audioReuseIdentifier , for: indexPath) as! UICollectionViewCell
+            return cell
+        case 5:
+            print("Discussion")
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: discussionReuseIdentifier , for: indexPath) as! UICollectionViewCell
+            return cell
+        case 6:
+            print("Expert Summary")
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: expertSummaryReuseIdentifier , for: indexPath) as! UICollectionViewCell
+            return cell
+        case 7:
+            print("External Links")
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: externalLinksReuseIdentifier , for: indexPath) as! UICollectionViewCell
+            return cell
+        case 8:
+            print("Qwk Recommendations")
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: qwkDescriptionReuseIdentifier , for: indexPath) as! UICollectionViewCell
+            return cell
+        default:
+            fatalError()
+
+        }
+        // This line will never be reached
+//        return UICollectionViewCell()
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
