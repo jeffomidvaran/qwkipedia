4e74da621d18d0a5edbedcd1a39b7b9d98310e94
lyfkfc <32535299+lyfkfc@users.noreply.github.com>
2021-05-25 00:36:48 -0700

    Import image from fb

--- qwkipedia_web/src/Database.js
+++ qwkipedia_web/src/Database.js
@@ -2,9 +2,13 @@ import Fire from './Fire';
 import {db} from './Fire';
 import React,{useState,useEffect} from 'react';
 import getYouTubeID from 'get-youtube-id';
+import YoutubeEmbed from "./YoutubeEmbed";
 
 function DataBase() {
     const [blogs, setBlogs] = useState([])
+    const [videoId, setVideoId] = useState("")
+    const [image1, setImage] = useState("")
+
     const fetchBlogs = async() => {
         const response = db.collection('users').doc('testUser1');
         const data = await response.get();
@@ -21,21 +25,36 @@ function DataBase() {
         } else {
           var videoLink = vdata.data()['youTubeURL'];
           console.log("video link: ", videoLink);
-          var videoId = getYouTubeID(videoLink);
-          console.log(videoId);
+          var videoId1 = getYouTubeID(videoLink);
+          console.log(videoId1);
+          setVideoId(videoId1);
         }
         // data.doc.forEach(item=>{
         //     setBlogs([...blogs, item.data()])
         // })
     }
 
+    const fetchImages = () => {
+      let imageRef = Fire.storage().ref("/puppy1.jpeg");
+      imageRef
+        .getDownloadURL()
+        .then((url) => {
+          setImage(url);
+        })
+        .catch((e) => console.log('getting downloadURL of image error => ', e));
+    }
+
     useEffect(() => {
         fetchBlogs();
+        fetchImages();
     }, [])
 
     return (
         <div className="App">
           DB
+          {/* {videoId} */}
+          <YoutubeEmbed embedId={videoId} />
+          <img src={image1}  />
           {/* {
             blogs && blogs.map(blog=>{
               return(
